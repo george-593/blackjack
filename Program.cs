113b68b01abe75157340b85b645054681c62f65a
george-593 <63100333+george-593@users.noreply.github.com>
2025-10-20 16:06:58 +0100

    Function to generate a random card

--- Program.cs
+++ Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿Card GenerateRandomCard()
+{
+    Random random = new Random();
+    string[] suits = { "Spades", "Hearts", "Diamonds", "Clubs" };
+    int suitNum = random.Next(0, suits.Length);
+    string suit = suits[suitNum];
+
+    int cardNum = random.Next(1, 14);
+
+    return new Card(suit, cardNum);
+}
--- obj/Debug/net8.0/Blackjack.AssemblyInfo.cs
+++ obj/Debug/net8.0/Blackjack.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Blackjack")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab12e5eb73bfce1eb20936a44c2902f99bc1e31b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+988e19c6cbda41601c7bcc7e3033e9fc930a5b01")]
 [assembly: System.Reflection.AssemblyProductAttribute("Blackjack")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Blackjack")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
