5be6ef63622fa19252bcacaa55cdaf1e21c84f57
george-593 <63100333+george-593@users.noreply.github.com>
2025-10-20 16:21:30 +0100

    Remove uncecesarry random class and add comments

--- Program.cs
+++ Program.cs
@@ -2,11 +2,12 @@
 
 Card GenerateRandomCard()
 {
-    Random random = new Random();
+    // Get a random card from the suits
     string[] suits = { "Spades", "Hearts", "Diamonds", "Clubs" };
     int suitNum = RandomNumberGenerator.GetInt32(0, suits.Length);
     string suit = suits[suitNum];
 
+    // Random card number from the blackjack deck
     int cardNum = RandomNumberGenerator.GetInt32(1, 13);
 
     return new Card(suit, cardNum);
